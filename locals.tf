04ef78c849cb87a193218730676c38d6f8d3f8e7
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-04-16 11:31:31 +0530

    concat error fix

--- locals.tf
+++ locals.tf
@@ -1,3 +1,3 @@
 locals {
-   all_private_subnet_ids = concat(length(module.subnets["db"].route_table_ids),length(module.subnets["app"].route_table_ids),length(module.subnets["web"].route_table_ids))
+   all_private_subnet_ids = concat(module.subnets["db"].route_table_ids,module.subnets["app"].route_table_ids,module.subnets["web"].route_table_ids)
 }
